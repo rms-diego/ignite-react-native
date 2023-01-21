cfb1ec4b4c1ba2b15d4c4cfb5f96201cf48ac5e6
Diego Ramos <diegorbrito9@gmail.com>
2023-01-21 19:07:35 -0300

    [ignite] [react native] [fundamentos react native] [fundamentos]  fim da aula 'Entendendo jsx'

--- fundamentos-react-native/App.tsx
+++ fundamentos-react-native/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Olá mundo !</Text>
+      <Text style={styles.text}>Teste</Text>
     </View>
   );
 }
@@ -13,8 +12,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131016",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    color: "white",
   },
 });
