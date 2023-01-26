26b6d1b8ae4b69c47cb7e7784d2fe6bb02e7e76f
Diego Ramos <diegorbrito9@gmail.com>
2023-01-26 14:39:09 -0300

    [ignite] [react native] [Criando interfaces & navegação] [css in Js] fim da aula 'Definindo o tema da aplicação'

--- criando-interfaces-e-navegacao/App.tsx
+++ criando-interfaces-e-navegacao/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
 
 import { Groups } from "@screens/Groups";
 
+import theme from "./src/styles";
+
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar />
-    </>
+    </ThemeProvider>
   );
 }
