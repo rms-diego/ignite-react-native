ac0250919cea521ea5ef55ea30f76a14b98bf071
Diego Ramos <diegorbrito9@gmail.com>
2023-01-26 12:59:21 -0300

    [ignite] [react native] [Criando interfaces & navegação] [Estrutura da aplicção] fim da aula 'Organizando e executando o projeto'

---
+++ criando-interfaces-e-navegacao/src/screens/Groups/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
