c3169c0b2e9f1094bf24bfdb8de4934b86e40dcf
Diego Ramos <diegorbrito9@gmail.com>
2023-01-26 19:10:10 -0300

    [ignite] [react native] [Criando interfaces & navegação] [css in Js] fim da aula 'Fonts personalizadas'

--- criando-interfaces-e-navegacao/App.tsx
+++ criando-interfaces-e-navegacao/App.tsx
@@ -1,15 +1,22 @@
-import { StatusBar } from "react-native";
+import { ActivityIndicator, StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import { Groups } from "@screens/Groups";
 
 import theme from "./src/styles";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
-      <StatusBar />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar translucent backgroundColor={theme.COLORS.GRAY_600} />
     </ThemeProvider>
   );
 }
