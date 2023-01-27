644860e6fc3ad9366adbabc83475ced5184a847f
Diego Ramos <diegorbrito9@gmail.com>
2023-01-27 07:37:41 -0300

    [ignite] [react native] [Criando interfaces & navegação] [css in Js] fim da aula 'Criando o componente highlight'

--- criando-interfaces-e-navegacao/src/screens/Groups/index.tsx
+++ criando-interfaces-e-navegacao/src/screens/Groups/index.tsx
@@ -1,11 +1,14 @@
 import { Container } from "./styles";
 
 import { Header } from "@components/Header";
+import { Highlight } from "@components/highligth";
 
 export function Groups() {
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
     </Container>
   );
 }
